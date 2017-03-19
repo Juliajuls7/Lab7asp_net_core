cbf209fa788352fe4d5366f7bc6d10da08d72634
Juliajuls7 <julya-ponasenkova@mail.ru>
2017-03-19 20:58:58 +0500

    Start

---
+++ src/WebApplication5/MyMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication5
+{
+    public class MyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public MyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string path = context.Request.Path.Value.ToLower();
+            switch (path) {
+                case "/test":
+                    await context.Response.WriteAsync("Test Page");
+                    break;
+
+                case "/hello":
+                    await context.Response.WriteAsync("Hello Page");
+                    break;
+
+                default:
+                    await context.Response.WriteAsync("Default Page");
+                    break;
+            }            
+        }
+    }
+}
